05e157a56b17dd851e408b6c2921f0e0d6297cd4
wangbiao <wangbiaoneu@sina.com>
2018-10-18 20:00:02 +0800

    正则表达式匹配，需要用动态规划

--- src/regularExpressionMatching.go
+++ src/regularExpressionMatching.go
@@ -1,21 +1,32 @@
 package main
 
-func main() {}
+func main() {
+	println(isMatch("ab", "a*"))
+	println(isMatch("ab", ".*"))
+	println(isMatch("ab", "a"))
+	println(isMatch("aab", "c*a*b"))
+	println(isMatch("mississippi", "mis*is*p*."))
+}
 
 /**
 https://leetcode.com/problems/regular-expression-matching/
+
+https://leetcode.com/problems/regular-expression-matching/solution/
 */
 func isMatch(s string, p string) bool {
 	var i, j = 0, 0
 	var match string
 	var repeat = false
-	var a, b uint8
-	for ; i < len(s) && j < len(p); i++ {
-		a, b = s[i], p[j]
+	var a, b uint8 = s[i], p[j]
+	for ; i < len(s); i++ {
+		a = s[i]
 		if '.' == b {
 			b = a
 			match += string(b)
 			j++
+			if j < len(p) {
+				b = p[j]
+			}
 			continue
 		}
 		if '*' == b {
@@ -24,14 +35,32 @@ func isMatch(s string, p string) bool {
 			}
 			repeat = true
 			b = match[j-1]
+			i--
 			continue
 		} else {
 			repeat = false
 		}
 		if a == b {
 			match += string(b)
-
+			if repeat {
+				continue
+			} else {
+				j++
+				if j < len(p) {
+					b = p[j]
+				}
+			}
+		} else {
+			i--
+			j++
+			if j < len(p) {
+				b = p[j]
+			}
 		}
 	}
-	return true
+	if i < len(s) {
+		return false
+	} else {
+		return true
+	}
 }
