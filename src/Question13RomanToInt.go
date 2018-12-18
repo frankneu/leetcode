628b5b467d867cc8b5386cfaf2b00cde31480b27
wangbiao <wangbiaoneu@sina.com>
2018-12-18 15:25:06 +0800

    https://leetcode.com/problems/roman-to-integer/

--- src/Question13RomanToInt.go
+++ src/Question13RomanToInt.go
@@ -1,1 +1,40 @@
 package main
+
+/**
+https://leetcode.com/problems/roman-to-integer/
+*/
+
+var ROMAN_MAP = map[string]int{
+	"M":  1000,
+	"CM": 900,
+	"D":  500,
+	"CD": 400,
+	"C":  100,
+	"XC": 90,
+	"L":  50,
+	"XL": 40,
+	"X":  10,
+	"IX": 9,
+	"V":  5,
+	"IV": 4,
+	"I":  1,
+}
+
+func romanToInt(s string) int {
+	result := 0
+	i := 0
+	for i < len(s) {
+		if i < len(s)-1 && ((s[i] == 'I' && (s[i+1] == 'V' || s[i+1] == 'X')) || (s[i] == 'X' && (s[i+1] == 'L' || s[i+1] == 'C')) || (s[i] == 'C' && (s[i+1] == 'D' || s[i+1] == 'M'))) {
+			result += ROMAN_MAP[string([]byte{s[i], s[i+1]})]
+			i += 2
+			continue
+		}
+		result += ROMAN_MAP[string([]byte{s[i]})]
+		i++
+	}
+	return result
+}
+
+func main() {
+	println(romanToInt("MCMXCIV"))
+}
