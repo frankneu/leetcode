6628d6a41c362b43cf0f4c5defb99cef13fe1305
wangbiao <wangbiaoneu@sina.com>
2019-01-22 20:24:46 +0800

    补充归并排序算法Go实现

--- src/sort/MergeSort.go
+++ src/sort/MergeSort.go
@@ -2,21 +2,25 @@ package main
 
 import "math"
 
-func merge(arr []int, left, pivotal, right int) {
-	nums1 := make([]int, pivotal-left+1)
-	nums2 := make([]int, right-pivotal+1)
-	copy(nums1, arr[left:pivotal])
-	copy(nums2, arr[pivotal:right])
-	nums1[len(nums1)-1] = math.MaxInt32
-	nums2[len(nums2)-1] = math.MaxInt32
-	i := 0
-	j := 0
-	for k := left; k < right; k++ {
-		if nums1[i] < nums2[j] {
-			arr[k] = nums1[i]
+/**
+左右都有序的数组进行合并
+*/
+func merge(arr []int, low, mid, high int) {
+	leftLen := mid - low + 1
+	rightLen := high - mid
+	arrLeft := make([]int, leftLen+1)
+	copy(arrLeft, arr[low:mid+1])
+	arrLeft[leftLen] = math.MaxInt32 //哨兵牌
+	arrRight := make([]int, rightLen+1)
+	copy(arrRight, arr[mid+1:high+1])
+	arrRight[rightLen] = math.MaxInt32 //哨兵牌
+	i, j := 0, 0
+	for k := low; k <= high; k++ {
+		if arrLeft[i] <= arrRight[j] {
+			arr[k] = arrLeft[i]
 			i++
 		} else {
-			arr[k] = nums2[j]
+			arr[k] = arrRight[j]
 			j++
 		}
 	}
@@ -32,7 +36,9 @@ func mergeSort(arr []int, left, right int) {
 }
 
 func main() {
-	nums := []int{1, 2, 6, 7, 4, 3, 9, 8}
+	nums := []int{7, 6, 2, 1, 8, 5, 3}
+	//nums2 := []int{1, 2, 6, 7, 4, 3, 9, 8}
+	//merge(nums1, 0, 4, 6)
 	mergeSort(nums, 0, len(nums)-1)
 	for i := 0; i < len(nums); i++ {
 		println(nums[i])
