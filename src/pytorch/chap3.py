06d9a098a00496bcd8ca8c1a59e04d4f8a191397
frank <wangbiaoneu@sina.com>
2023-04-13 18:10:50 +0800

    pytorch chapter 3

--- src/pytorch/chap3.py
+++ src/pytorch/chap3.py
@@ -39,4 +39,36 @@ vt_sum_multiply = vt_multiply.sum("y")
 print(vt_sum_multiply.size(), vt_sum_multiply.names)
 
 storage = vt_sum_multiply.untyped_storage()
-print(storage)
+
+points = vt_sum_multiply[1]
+
+print("before", points.size())
+# stride是在指定维度dim中从一个元素跳到下一个元素所必需的步长。当没有参数传入时，返回所有步长的元组。否则，将返回一个整数值作为特定维度dim中的步长。
+# （5*6*7, 6*7, 7, 1）
+stride = points.stride()
+
+print("stride", stride)
+
+test_trans = torch.rand(2,3,4,5)
+print(test_trans.size())
+transposed = test_trans.transpose(0,2)
+print(transposed.size())
+# 某些Tensor操作（如transpose、permute、narrow、expand）与原Tensor是共享内存中的数据，不会改变底层数组的存储，但原来在语义上相邻、内存里也相邻的元素在执行这样的操作后，在语义上相邻，但在内存不相邻，即不连续了（is not contiguous）。
+print(test_trans.is_contiguous())
+print(transposed.is_contiguous())
+contiguous = transposed.contiguous()
+print(contiguous.is_contiguous())
+
+torch.save(contiguous, "contiguous.t")
+
+import h5py
+file_w = h5py.File("contiguous.hdf5", "w")
+file_w.create_dataset("contiguous", data = contiguous.numpy())
+file_w.close()
+
+file_r = h5py.File("contiguous.hdf5", "r")
+file_read_numpy = file_r["contiguous"]
+# ???
+tensor_from_h5py = torch.from_numpy(file_read_numpy[-2 :])
+print(tensor_from_h5py.size())
+file_r.close()
